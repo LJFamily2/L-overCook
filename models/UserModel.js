c7e3c349a44f69bd71a0a783ff109eb9d078fe9c
s3977794 <s3977794@rmit.edu.vn>
2024-03-26 15:25:57 +0700

    Setup User Model

---
+++ models/UserModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+  role: Boolean,
+  createAt: Date,
+});
+
+const UserModel = mongoose.model("Users", UserSchema);
+module.exports = UserModel;
