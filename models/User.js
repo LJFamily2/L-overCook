103173c19c91f11db55de427469112b9c202022d
s3977794 <s3977794@rmit.edu.vn>
2024-04-09 00:21:36 +0700

    Add user management FE

--- models/User.js
+++ models/User.js
@@ -20,5 +20,27 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.methods.getFormattedDateTime = function () {
+  const dateOptions = { day: "2-digit", month: "2-digit", year: "numeric" };
+  const timeOptions = { hour: "2-digit", minute: "2-digit", second: "2-digit" };
+
+  // Check if createdAt field is defined
+  if (this.createdAt) {
+    const formattedDate = this.createdAt.toLocaleDateString(
+      "en-GB",
+      dateOptions
+    );
+    const formattedTime = this.createdAt.toLocaleTimeString(
+      "en-US",
+      timeOptions
+    );
+    return `${formattedDate}\n ${formattedTime}`;
+  } else {
+    return "";
+  }
+};
+
 const UserModel = mongoose.model("Users", userSchema);
+
+
 module.exports = UserModel;
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ const cuisine = require('./cuisineRoute');
 const ingredient = require('./ingredientRoute');
 const recipe = require('./recipeRoute');
 const searchHistory = require('./clients/searchHistoryRoute');
-const favoriteRecipe = require('./clients/favoriteRecipeRoute')
-
+const favoriteRecipe = require('./clients/favoriteRecipeRoute');
+const userManagement = require('./admin/userManagementRoute');
 
 const routes = [
     {path: '/', route: home},
@@ -21,7 +21,8 @@ const routes = [
     {path: '/ingredient', route: ingredient},
     {path: '/recipe', route: recipe},
     {path: '/searchHistory', route: searchHistory},
-    {path: '/favoriteRecipe', route: favoriteRecipe}
+    {path: '/favoriteRecipe', route: favoriteRecipe},
+    {path: '/userManagement', route: userManagement}
 ];
 
 module.exports = routes;
