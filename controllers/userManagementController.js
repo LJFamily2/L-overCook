9950f6381027ee74b1349466d346410ff12d19b8
s3977794 <s3977794@rmit.edu.vn>
2024-04-12 02:11:03 +0700

    Setup Nodemailer

---
+++ controllers/userManagementController.js
@@ -0,0 +1,60 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+const updateUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const existingUser = await User.findById(userId);
+
+    if (!existingUser) {
+      req.flash("error", "User not found");
+      return res.status(404).send("User not found");
+    }
+
+    let updateFields = {
+      username: req.body.username || existingUser.username,
+      email: req.body.email || existingUser.email,
+      role: req.body.role || existingUser.role,
+    };
+
+    if (req.body.password) {
+      const hashPassword = await bcrypt.hash(req.body.password, 10);
+      updateFields.password = hashPassword;
+    } else {
+      updateFields.password = existingUser.password;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(userId, updateFields, {
+      new: true,
+    });
+
+    if(!updatedUser) {
+      res.send("Code ngu, sửa lại đi");
+      req.flash("error", "Error updating user");
+      return res.status(500).send("Error updating user");
+    }
+    req.flash("success", "User updated successfully");
+    return res.send("User updated successfully");
+  } catch (error) {
+    console.error("Error updating user:", error);
+    return res.status(500).send("Internal server error");
+  }
+};
+
+// Controller for deleting a user
+const deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      req.flash("error", "User not found");
+      return res.status(404);
+    }
+    return res.send("User deleted successfully");
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    return res.status(500);
+  }
+};
+
+module.exports = { updateUser, deleteUser };
