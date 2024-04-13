1a1ec72b2628d737654f53c70f8dc6e83e5885a2
s3977794 <s3977794@rmit.edu.vn>
2024-04-13 11:45:44 +0700

    Reset Password feature (tested)

--- controllers/client/profileController.js
+++ controllers/client/profileController.js
@@ -1,10 +1,16 @@
 const crypto = require('crypto');
 const UserModel = require('../../models/User');
 const sendMailResetPassword = require('../sendEmail');
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require('uuid');
 
+// Render get profile page
+const getProfilePage = async (req, res) => {
+   res.send('Profile page');
+};
+
+// Render get email page
 const getEmailPage = async (req, res) => {
-   res.send('Email page');
+   res.render('emailPageTesting', { layout: false, user: req.user });
 };
 
 // Function to generate OTP and timestamp
@@ -14,31 +20,55 @@ const generateOTP = () => {
    return { otp, timestamp };
 };
 
+// Function to generate and send OTP email
+const generateAndSendOtpEmail = async (user) => {
+   try {
+      const { otp, timestamp } = generateOTP();
+      
+      // Update user model with OTP and its timestamp
+      await UserModel.findOneAndUpdate(
+         { _id: user._id },
+         { otp, otpTimestamp: timestamp }
+      );
+
+      // Send email with OTP
+      await sendMailResetPassword(user.email, otp);
+
+      return { otp, timestamp };
+   } catch (error) {
+      console.error(error);
+      res.status(500);
+   }
+};
+
 // Function to send email with OTP
 const sendOtp = async (req, res) => {
    const { email } = req.body;
 
    try {
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+         res.send('User not found').status(404);
+         return;
+      }
+
       // Generate OTP and timestamp
-      const { otp, timestamp } = generateOTP();
+      await generateAndSendOtpEmail(user);
 
-      // Update user model with OTP and its timestamp
-      await UserModel.findOneAndUpdate(
-         { email },
-         { otp, otpTimestamp: timestamp }
+      res.redirect(
+         `/account/profile/resetPassword/verifyOTP?token=${user.token}`
       );
-
-      // Send email with OTP
-      await sendMailResetPassword(email, otp);
-
-      res
-         .send('OTP page')
-         .redirect(`/account/resetPassword?token=${req.user.token}`);
       // .status(200);
    } catch (error) {
       console.error(error);
       res.status(500);
    }
+};
+
+const verifyOtpPage = async (req, res) => {
+   const { token } = req.query;
+
+   res.render('verifyOTPTesting', { layout: false, token });
 };
 
 // Function to verify OTP expiration
@@ -57,39 +87,62 @@ const checkOtp = (otpData, userEnteredOTP) => {
 };
 
 const verifyOtp = async (req, res) => {
-   const { userEnteredOTP } = req.body;
-   const token = req.params.token;
-
    try {
+      const { otp, token } = req.body;
       const user = await UserModel.findOne({ token });
-      if (!user){
+      if (!user) {
          res.send('User not found').status(404);
          return;
       }
 
       // Check if user data exists and if OTP verification is successful
-      if (
-         checkOtp(
-            { otp: user.otp, timestamp: user.otpTimestamp },
-            userEnteredOTP
-         )
-      ) {
+      if (checkOtp({ otp: user.otp, timestamp: user.otpTimestamp }, otp)) {
          // Generate new token and remove the OTP and its timestamp
          const newToken = uuidv4();
          await UserModel.findOneAndUpdate(
             { _id: user._id },
-            { token: newToken , $unset: { otp: 1, otpTimestamp: 1 } }
+            { token: newToken, $unset: { otp: 1, otpTimestamp: 1 } }
          );
          res.send('OTP verified successfully').status(200);
-         
       } else {
-         res.send(
-            'Invalid OTP'
-         ).redirect(`/account/resetPassword?token=${user.token}`);
+         res.redirect(
+            `/account/profile/resetPassword/verifyOTP?token=${user.token}`
+         );
       }
    } catch (err) {
       console.error(err);
       res.status(500);
    }
 };
-module.exports = { getEmailPage, sendOtp, verifyOtp };
+
+// Resend the verification code if the user has not received it
+
+const resendOtp = async (req, res) => {
+   try {
+      const { token } = req.body;
+
+      const user = await UserModel.findOne({ token });
+      if (!user) {
+         res.send('User not found').status(404);
+         return;
+      }
+
+     // Generate OTP and timestamp
+     await generateAndSendOtpEmail(user);
+
+      res.redirect(
+         `/account/profile/resetPassword/verifyOTP?token=${user.token}`
+      );
+   } catch (error) {
+      console.error(error);
+      res.status(500);
+   }
+};
+module.exports = {
+   verifyOtpPage,
+   getProfilePage,
+   getEmailPage,
+   sendOtp,
+   verifyOtp,
+   resendOtp,
+};
--- routes/partials/profileRoute.js
+++ routes/partials/profileRoute.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const profileController = require('../../controllers/client/profileController');
 
-router.get('/resetPassword', profileController.getEmailPage);
-router.post('/resetPassword', profileController.sendOtp);
-router.post('/resetPassword/verifyOTP', profileController.verifyOtp);
+router.get('/profile', profileController.getProfilePage);
+router.get('/profile/resetPassword', profileController.getEmailPage);
+router.post('/profile/resetPassword', profileController.sendOtp);
+
+router.get('/profile/resetPassword/verifyOTP', profileController.verifyOtpPage);
+router.post('/profile/resetPassword/verifyOTP', profileController.verifyOtp);
 
 module.exports = router;
