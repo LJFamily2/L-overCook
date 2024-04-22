7760ad8f12766a5f2fda2ed6fc58535d1775d88b
s3977794 <s3977794@rmit.edu.vn>
2024-04-23 01:55:09 +0700

    Implementing recipe detail page

--- controllers/admin/recipeController.js
+++ controllers/admin/recipeController.js
@@ -5,33 +5,68 @@ const Cuisine = require('../../models/Cuisine');
 const ingredientController = require('./ingredientController');
 const cuisineController = require('./cuisineController');
 
-
 // Get all recipes
 exports.getAllRecipes = async (req, res) => {
    try {
       const recipes = await Recipe.find()
-      .populate({
-         path: 'ingredients.ingredient',
-         select: 'name -_id',
-      })
-      .populate({
-         path: 'cuisine',
-         select: 'name -_id', 
-      })
-      .exec();
+         .populate({
+            path: 'ingredients.ingredient',
+            select: 'name -_id',
+         })
+         .populate({
+            path: 'cuisine',
+            select: 'name -_id',
+         })
+         .exec();
       return recipes;
    } catch (error) {
       throw new Error(error.message);
    }
 };
 
+// Recipe detail page
+exports.getRecipeDetailPage = async (req, res) => {
+   try {
+      // Find the recipe by its slug and populate it with its related fields
+      const recipe = await Recipe.findOne({ slug: req.params.slug })
+         .populate({
+            path: 'ingredients.ingredient',
+            select: 'name -_id',
+         })
+         .populate({
+            path: 'cuisine',
+            select: 'name -_id',
+         })
+         .exec();
+         if (!recipe) {
+            res.status(404).redirect('/')
+            return; 
+         }
+
+         res.render('client/recipeDetail', {
+            layout: './layouts/client/defaultLayout',
+            recipe,
+            userAuthentication: false,
+            user: req.user,
+         });
+   } catch (error) {
+      console.log(error)
+   }
+};
 // Get all recipes
 exports.getRecipePage = async (req, res) => {
    try {
       const recipes = await this.getAllRecipes();
       const ingredients = await ingredientController.getAllIngredients();
       const cuisines = await cuisineController.getAllCuisines();
-      res.render('admin/recipeManagementPage', { recipes, ingredients, cuisines, layout: "./layouts/admin/defaultLayout", currentPage: 'recipe-management', heading: 'Recipe Management'});
+      res.render('admin/recipeManagementPage', {
+         recipes,
+         ingredients,
+         cuisines,
+         layout: './layouts/admin/defaultLayout',
+         currentPage: 'recipe-management',
+         heading: 'Recipe Management',
+      });
    } catch (error) {
       throw new Error(error.message);
    }
@@ -42,23 +77,22 @@ exports.getRecipeData = async (req, res) => {
    try {
       // Extract the recipe ID from the request parameters
       const recipeId = req.params.id;
-  
+
       // Fetch the recipe data from the database
       const recipe = await Recipe.findById(recipeId);
-  
+
       // If the recipe is found, send it as a JSON response
       if (recipe) {
-        res.json(recipe);
+         res.json(recipe);
       } else {
-        // If the recipe is not found, send a 404 response
-        res.status(404).json({ message: 'Recipe not found' });
+         // If the recipe is not found, send a 404 response
+         res.status(404).json({ message: 'Recipe not found' });
       }
    } catch (error) {
       // If there's an error, send a 500 response
       res.status(500).json({ message: 'Server error' });
    }
 };
-
 
 // Helper function to create recipe without returning status
 exports.createRecipeLogic = async (
@@ -71,7 +105,6 @@ exports.createRecipeLogic = async (
    url
 ) => {
    try {
-      
       // Check if recipe existed
       const existingRecipe = await Recipe.findOne({ name });
       if (existingRecipe) {
@@ -87,11 +120,15 @@ exports.createRecipeLogic = async (
       for (const ingredient of ingredients) {
          let ingredientId = ingredientMap.get(ingredient.name);
          if (!ingredientId) {
-            throw new Error('Ingredient "' + ingredient.name + '" not found. Please ensure that all ingredients are present in the database before creating new recipe. <a href="/ingredientManagement">View ingredient database.</a>');
+            throw new Error(
+               'Ingredient "' +
+                  ingredient.name +
+                  '" not found. Please ensure that all ingredients are present in the database before creating new recipe. <a href="/ingredientManagement">View ingredient database.</a>'
+            );
          }
          ingredient.ingredient = ingredientId;
       }
-      
+
       // Create a new Recipe instance
       const recipe = new Recipe({
          name,
@@ -113,17 +150,25 @@ exports.createRecipeLogic = async (
 
 // Create new ingredient by calling the helper function and return status
 exports.createRecipe = async (req, res) => {
-   const { name, description, ingredient, quantity, cuisine, image, time, url } = req.body;
+   const {
+      name,
+      description,
+      ingredient,
+      quantity,
+      cuisine,
+      image,
+      time,
+      url,
+   } = req.body;
 
    // Ensure ingredient and quantity are arrays and map ingredient name with quantity
-   const ingredients = Array.isArray(ingredient) ? 
-       ingredient.map((name, index) => ({ name, quantity: quantity[index] })) :
-       [{ name: ingredient, quantity }];
+   const ingredients = Array.isArray(ingredient)
+      ? ingredient.map((name, index) => ({ name, quantity: quantity[index] }))
+      : [{ name: ingredient, quantity }];
 
    console.log(ingredients);
 
    try {
-      
       const newRecipe = await this.createRecipeLogic(
          name,
          description,
@@ -134,25 +179,37 @@ exports.createRecipe = async (req, res) => {
          url
       );
       if (newRecipe) {
-         res.redirect('/recipeManagement?success=Recipe+added+successfully'); 
+         res.redirect('/recipeManagement?success=Recipe+added+successfully');
       }
    } catch (error) {
-      res.redirect('/recipeManagement?error=true&message=' + encodeURIComponent(error.message));
+      res.redirect(
+         '/recipeManagement?error=true&message=' +
+            encodeURIComponent(error.message)
+      );
    }
 };
 
-exports.updateRecipe = async(req, res) => {
+exports.updateRecipe = async (req, res) => {
    const recipeId = req.params.id;
-   console.log('Recipe ID: ', recipeId)
-   const { name, description, ingredient, quantity, cuisine, image, time, url } = req.body;
-   
+   console.log('Recipe ID: ', recipeId);
+   const {
+      name,
+      description,
+      ingredient,
+      quantity,
+      cuisine,
+      image,
+      time,
+      url,
+   } = req.body;
+
    console.log(ingredient);
    console.log(quantity);
 
    // Ensure ingredient and quantity are arrays and map ingredient name with quantity
-   const ingredients = Array.isArray(ingredient) ? 
-       ingredient.map((name, index) => ({ name, quantity: quantity[index] })) :
-       [{ name: ingredient, quantity }];
+   const ingredients = Array.isArray(ingredient)
+      ? ingredient.map((name, index) => ({ name, quantity: quantity[index] }))
+      : [{ name: ingredient, quantity }];
 
    console.log(ingredients);
 
@@ -165,66 +222,82 @@ exports.updateRecipe = async(req, res) => {
    for (const ingredient of ingredients) {
       let ingredientId = ingredientMap.get(ingredient.name);
       if (!ingredientId) {
-         throw new Error('Ingredient "' + ingredient.name + '" not found. Please ensure that all ingredients are present in the database before creating new recipe. <a href="/ingredientManagement">View ingredient database.</a>');
+         throw new Error(
+            'Ingredient "' +
+               ingredient.name +
+               '" not found. Please ensure that all ingredients are present in the database before creating new recipe. <a href="/ingredientManagement">View ingredient database.</a>'
+         );
       }
       ingredient.ingredient = ingredientId;
    }
 
    // Check if category existed
    let cuisineId;
-   const cuisineMap = new Map((await Cuisine.find()).map(cui => [cui.name, cui._id]));
+   const cuisineMap = new Map(
+      (await Cuisine.find()).map((cui) => [cui.name, cui._id])
+   );
    if (cuisineMap.has(cuisine)) {
-       // If the category exists, retrieve its ID from the map
-       cuisineId = cuisineMap.get(cuisine);
+      // If the category exists, retrieve its ID from the map
+      cuisineId = cuisineMap.get(cuisine);
    } else {
-       throw new Error('Cuisine "' + cuisine + '" not found.')
+      throw new Error('Cuisine "' + cuisine + '" not found.');
    }
-   
-   try{
-      console.log('Looking for recipe with matching id...')
+
+   try {
+      console.log('Looking for recipe with matching id...');
       const recipe = await Recipe.findById(recipeId);
-      if(!recipe){
-         throw new Error('Failed to update item: Recipe not found.')
+      if (!recipe) {
+         throw new Error('Failed to update item: Recipe not found.');
       }
-      console.log('Recipe found, checking if updated name exist...')
+      console.log('Recipe found, checking if updated name exist...');
 
       // Check if name already exists for another ingredient
-      const existingRecipe = await Recipe.findOne({name});
+      const existingRecipe = await Recipe.findOne({ name });
       if (existingRecipe && existingRecipe._id.toString() !== recipeId) {
-         throw new Error(`Failed to update item: Recipe with name '${name}' already exists`);
-     }
+         throw new Error(
+            `Failed to update item: Recipe with name '${name}' already exists`
+         );
+      }
 
-      console.log('Updating recipe...')
-      const resultRecipe = await Recipe.findByIdAndUpdate(recipeId, { 
-         name: name,
-         description: description,
-         ingredients: ingredients,
-         cuisine: cuisineId,
-         image: image,
-         time: time,
-         url: url
-      }, { new: true });
+      console.log('Updating recipe...');
+      const resultRecipe = await Recipe.findByIdAndUpdate(
+         recipeId,
+         {
+            name: name,
+            description: description,
+            ingredients: ingredients,
+            cuisine: cuisineId,
+            image: image,
+            time: time,
+            url: url,
+         },
+         { new: true }
+      );
       console.log('results: ', resultRecipe);
       res.redirect('/recipeManagement?success=Recipe+updated+successfully');
+   } catch (error) {
+      console.log('Error updating recipe.');
+      res.redirect(
+         '/recipeManagement?error=true&message=' +
+            encodeURIComponent(error.message)
+      );
+   }
+};
 
-   }catch(error){
-      console.log('Error updating recipe.')
-      res.redirect('/recipeManagement?error=true&message=' + encodeURIComponent(error.message));        
-  }
-}
-
-exports.deleteRecipe = async(req, res)=>{
+exports.deleteRecipe = async (req, res) => {
    const recipeId = req.params.id;
-   try{
+   try {
       const recipe = await Recipe.findById(recipeId);
-      if(!recipe){
+      if (!recipe) {
          throw new Error('Failed to delete item: Recipe not found');
       }
 
-      await Recipe.deleteOne({_id: recipeId});
+      await Recipe.deleteOne({ _id: recipeId });
       res.redirect('/recipeManagement?success=Recipe+deleted+successfully');
-      
-   }catch(error){
-      res.redirect('/recipeManagement?error=true&message=' + encodeURIComponent(error.message));
-  }
-}
+   } catch (error) {
+      res.redirect(
+         '/recipeManagement?error=true&message=' +
+            encodeURIComponent(error.message)
+      );
+   }
+};
