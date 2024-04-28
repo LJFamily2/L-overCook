6b42e1f210e8ae00b0cf9bf831f8e173863411f4
s3977794 <s3977794@rmit.edu.vn>
2024-04-28 22:55:36 +0700

    Search feature Javascript

---
+++ public/js/clients/searchItems.js
@@ -0,0 +1,30 @@
+function searchItems(inputId, list, containerID) {
+   const input = document.getElementById(inputId);
+   const searchTerm = input.value.toLowerCase();
+   const items = document.getElementById(list).getElementsByTagName('li');
+   const searchDropDown = document.getElementById(containerID);
+   let timeout;
+   const debouncedSearch = () => {
+      if (searchTerm) {
+         searchDropDown.classList.remove('hidden');
+         for (const result of items) {
+            const item = result.textContent.toLowerCase();
+            if (item.includes(searchTerm)) {
+               result.style.display = 'block';
+            } else {
+               result.style.display = 'none';
+            }
+         }
+      } else {
+         searchDropDown.classList.add('hidden');
+      }
+   };
+   clearTimeout(timeout);
+
+   setTimeout(debouncedSearch, 100);
+}
+
+function setValue(inputId, value, list) {
+   document.getElementById(inputId).value = value;
+   document.getElementById(list).classList.toggle('hidden');
+}
