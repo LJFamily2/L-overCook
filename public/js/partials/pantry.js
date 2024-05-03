78a8095adabb3b26e4ec8c8227066edfbea70dbf
Vu Minh Ha <vuminhha2504@gmail.com>
2024-05-04 00:04:34 +0700

    Pantry animation

--- public/js/partials/pantry.js
+++ public/js/partials/pantry.js
@@ -4,5 +4,6 @@ let pantryContents = document.querySelector('.pantry-contents');
 
 pantryIcon.addEventListener('click', () => {
     pantryIcon.classList.toggle('moveUp');
+    pantryContents.classList.toggle('hidden');
     pantryContents.classList.toggle('show');
  });
