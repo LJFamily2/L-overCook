c94fb574971be685885422f7dd0789a3ee6b9890
s3977794 <s3977794@rmit.edu.vn>
2024-05-07 19:57:42 +0700

    Update ingredient pagination in admin

--- controllers/admin/ingredientController.js
+++ controllers/admin/ingredientController.js
@@ -8,58 +8,74 @@ const path = require('path');
 
 // Delete image path
 const deleteImageFile = async (image) => {
-    try {
-       await fs.unlink(path.join('public/uploadImages', image));
-    } catch (err) {
-       console.log('Error deleting image file:', err);
-    }
- };
+   try {
+      await fs.unlink(path.join('public/uploadImages', image));
+   } catch (err) {
+      console.log('Error deleting image file:', err);
+   }
+};
 
 // Get all ingredients
-exports.getAllIngredients = async (req,res) => {
-   try{
+exports.getAllIngredients = async (req, res) => {
+   try {
       const ingredients = await Ingredient.find()
-      .populate({
-          path: 'category',
-      })
-      .exec();
+         .populate({
+            path: 'category',
+         })
+         .exec();
       return ingredients;
-   }catch(error){
+   } catch (error) {
       throw new Error(error.message);
    }
 };
 
 // get all ingredients group with the category
 exports.getIngredientsForAllCategories = async (req, res) => {
-    try {
-        // Fetch all ingredients with their corresponding category populated
-        const ingredientsWithCategory = await Ingredient.find().populate('category');
+   try {
+      // Fetch all ingredients with their corresponding category populated
+      const ingredientsWithCategory = await Ingredient.find().populate(
+         'category'
+      );
 
-        // Group ingredients by category
-        const categoriesWithIngredients = {};
-        ingredientsWithCategory.forEach(ingredient => {
-            const categoryName = ingredient.category.name; 
-            if (!categoriesWithIngredients[categoryName]) {
-                categoriesWithIngredients[categoryName] = [];
-            }
-            categoriesWithIngredients[categoryName].push(ingredient.name);
-        });
+      // Group ingredients by category
+      const categoriesWithIngredients = {};
+      ingredientsWithCategory.forEach((ingredient) => {
+         const categoryName = ingredient.category.name;
+         if (!categoriesWithIngredients[categoryName]) {
+            categoriesWithIngredients[categoryName] = [];
+         }
+         categoriesWithIngredients[categoryName].push(ingredient.name);
+      });
 
-        return categoriesWithIngredients;
-    } catch (error) {
-        console.error('Error fetching categories with ingredients:', error);
-    }
+      return categoriesWithIngredients;
+   } catch (error) {
+      console.error('Error fetching categories with ingredients:', error);
+   }
 };
 
 // Get ingredient page
 exports.getIngredientPage = async (req, res) => {
    try {
-      const ingredients = await this.getAllIngredients();
+      const rowsPerPage = parseInt(req.query.rows) || 20;
+      const page = parseInt(req.query.page) || 1;
+      const limit = rowsPerPage;
+      const skip = (page - 1) * limit;
+      const ingredients = await Ingredient.find()
+         .skip(skip)
+         .limit(limit)
+         .populate({
+            path: 'category',
+         })
+         .exec();
+      const totalIngredient = await Ingredient.countDocuments();
       res.render('admin/ingredientManagementPage', {
          ingredients,
          layout: './layouts/admin/defaultLayout',
          currentPage: 'ingredient-management',
-         heading: 'Ingredient Management'
+         heading: 'Ingredient Management',
+         totalPages: Math.ceil(totalIngredient / limit),
+         currentPageNumber: page,
+         rowsPerPage: rowsPerPage,
       });
    } catch (error) {
       throw new Error(error.message);
@@ -68,83 +84,93 @@ exports.getIngredientPage = async (req, res) => {
 
 // Search for ingredients limit by 4
 
-
 // Helper function to create ingredient without returning status
-exports.createIngredientLogic = async(name, categoryName, image) => {
+exports.createIngredientLogic = async (name, categoryName, image) => {
    try {
-       // Check if ingredient exist
-       const existingIngredient = await Ingredient.findOne({ name });
-       if(existingIngredient){
-           throw new Error(`Failed to create item: Ingredient with name '${name}' already exists`);
-       }
-       console.log(existingIngredient)
-       
-       // Check if category exists
-       let categoryId;
-       const categoryMap = new Map((await Category.find()).map(cat => [cat.name, cat._id]));
-       if (categoryMap.has(categoryName)) {
-           // If the category exists, retrieve its ID from the map
-           categoryId = categoryMap.get(categoryName);
-       } else {
-           // If the category doesn't exist, create a new one
-           const newCategory = await categoryController.createCategoryLogic(categoryName, image);
-           categoryId = newCategory._id;
-       }
+      // Check if ingredient exist
+      const existingIngredient = await Ingredient.findOne({ name });
+      if (existingIngredient) {
+         throw new Error(
+            `Failed to create item: Ingredient with name '${name}' already exists`
+         );
+      }
+      console.log(existingIngredient);
 
-       // Create a new ingredient object with name and category
-       const ingredient = new Ingredient({
-           name,
-           category: categoryId,
-       });
+      // Check if category exists
+      let categoryId;
+      const categoryMap = new Map(
+         (await Category.find()).map((cat) => [cat.name, cat._id])
+      );
+      if (categoryMap.has(categoryName)) {
+         // If the category exists, retrieve its ID from the map
+         categoryId = categoryMap.get(categoryName);
+      } else {
+         // If the category doesn't exist, create a new one
+         const newCategory = await categoryController.createCategoryLogic(
+            categoryName,
+            image
+         );
+         categoryId = newCategory._id;
+      }
 
-       // Save the new ingredient to the database
-       const newIngredient = await ingredient.save();
-       return newIngredient;  
+      // Create a new ingredient object with name and category
+      const ingredient = new Ingredient({
+         name,
+         category: categoryId,
+      });
+
+      // Save the new ingredient to the database
+      const newIngredient = await ingredient.save();
+      return newIngredient;
    } catch (error) {
-       throw new Error(error.message);
+      throw new Error(error.message);
    }
 };
-
 
 // Create new ingredient by calling the helper function and return status
 exports.createIngredient = async (req, res) => {
    const { name, categoryName } = req.body;
-   try{
-       const image = req.file ? req.file.filename : null;
-       console.log(image)
-       const newIngredient = await this.createIngredientLogic(name, categoryName, image);
-       if(newIngredient){
-           res.redirect('/ingredients?success=Ingredient+added+successfully');
-       }
-   }catch(error){
-       res.redirect('/ingredients?error=true&message=' + encodeURIComponent(error.message));
+   try {
+      const image = req.file ? req.file.filename : null;
+      console.log(image);
+      const newIngredient = await this.createIngredientLogic(
+         name,
+         categoryName,
+         image
+      );
+      if (newIngredient) {
+         res.redirect('/ingredients?success=Ingredient+added+successfully');
+      }
+   } catch (error) {
+      res.redirect(
+         '/ingredients?error=true&message=' + encodeURIComponent(error.message)
+      );
    }
 };
 
 // Delete existing ingredient
-exports.deleteIngredient = async (req,res) => {
+exports.deleteIngredient = async (req, res) => {
    const ingredientId = req.params.id;
-   try{
-       // Check if ingredient exists
-       const ingredient = await Ingredient.findById(ingredientId);
-       if (!ingredient) {
-           throw new Error('Failed to delete item: Ingredient not found')
-       }
-       
-       // Delete the ingredient in  the recipe's ingredient array
-       await Recipe.updateMany(
-           { "ingredients.ingredient": ingredientId },
-           { $pull: { ingredients: { ingredient: ingredientId } } }
-       );
+   try {
+      // Check if ingredient exists
+      const ingredient = await Ingredient.findById(ingredientId);
+      if (!ingredient) {
+         throw new Error('Failed to delete item: Ingredient not found');
+      }
 
+      // Delete the ingredient in  the recipe's ingredient array
+      await Recipe.updateMany(
+         { 'ingredients.ingredient': ingredientId },
+         { $pull: { ingredients: { ingredient: ingredientId } } }
+      );
 
-       
-       // Delete ingredient
-       await Ingredient.deleteOne({ _id: ingredientId });
-       res.redirect('/ingredients?success=Ingredient+deleted+successfully');
-       
-   }catch(error){
-       res.redirect('/ingredients?error=true&message=' + encodeURIComponent(error.message));
+      // Delete ingredient
+      await Ingredient.deleteOne({ _id: ingredientId });
+      res.redirect('/ingredients?success=Ingredient+deleted+successfully');
+   } catch (error) {
+      res.redirect(
+         '/ingredients?error=true&message=' + encodeURIComponent(error.message)
+      );
    }
 };
 
@@ -177,76 +203,87 @@ exports.deleteIngredient = async (req,res) => {
 //             categoryId = newCategory._id;
 //         }
 //         console.log(req.file.filename)
-//         // Update image 
+//         // Update image
 //         if (req.file) {
 //             if (ingredient.categoryImage) {
 //                 await deleteImageFile(ingredient.categoryImage);
 //             }
 //             ingredient.categoryImage = req.file.filename;
 //         }
-        
+
 //         // Update the ingredient
 //         const resultIngredient = await Ingredient.findByIdAndUpdate(ingredientId, { name: name, category: categoryId, categoryImage: ingredient.categoryImage}, { new: true });
 //         res.redirect('/ingredients?success=Ingredient+updated+successfully');
-        
+
 //     }catch(error){
-//         res.redirect('/ingredients?error=true&message=' + encodeURIComponent(error.message));        
+//         res.redirect('/ingredients?error=true&message=' + encodeURIComponent(error.message));
 //     }
 // }
 
 // Update ingredient
 exports.updateIngredient = async (req, res) => {
-    const ingredientId = req.params.id;
-    const { name, categoryName } = req.body;
-    try {
-        // Check if id existed
-        const ingredient = await Ingredient.findById(ingredientId);
-        if (!ingredient) {
-            throw new Error('Failed to update item: Ingredient not found.');
-        }
+   const ingredientId = req.params.id;
+   const { name, categoryName } = req.body;
+   try {
+      // Check if id existed
+      const ingredient = await Ingredient.findById(ingredientId);
+      if (!ingredient) {
+         throw new Error('Failed to update item: Ingredient not found.');
+      }
 
-        // Check if name already exists for another ingredient
-        const existingIngredient = await Ingredient.findOne({ name });
-        if (existingIngredient && existingIngredient._id.toString() !== ingredientId) {
-            throw new Error(`Failed to update item: Ingredient with name '${name}' already exists`);
-        }
+      // Check if name already exists for another ingredient
+      const existingIngredient = await Ingredient.findOne({ name });
+      if (
+         existingIngredient &&
+         existingIngredient._id.toString() !== ingredientId
+      ) {
+         throw new Error(
+            `Failed to update item: Ingredient with name '${name}' already exists`
+         );
+      }
 
-        
-        // Check if category existed
-        let categoryId;
-        const categoryMap = new Map((await Category.find()).map(cat => [cat.name, cat._id]));
-        if (categoryMap.has(categoryName)) {
-            // If the category exists, retrieve its ID from the map
-            categoryId = categoryMap.get(categoryName);
-        } else {
-            const image = req.file ? req.file.filename : null;
-            // If the category doesn't exist, create a new one
-            const newCategory = await categoryController.createCategoryLogic(categoryName, image);
-            categoryId = newCategory._id;
-        }
+      // Check if category existed
+      let categoryId;
+      const categoryMap = new Map(
+         (await Category.find()).map((cat) => [cat.name, cat._id])
+      );
+      if (categoryMap.has(categoryName)) {
+         // If the category exists, retrieve its ID from the map
+         categoryId = categoryMap.get(categoryName);
+      } else {
+         const image = req.file ? req.file.filename : null;
+         // If the category doesn't exist, create a new one
+         const newCategory = await categoryController.createCategoryLogic(
+            categoryName,
+            image
+         );
+         categoryId = newCategory._id;
+      }
 
+      // Check if there are changes in the ingredient's name, category, or image
+      const updateFields = {};
+      if (name !== ingredient.name) {
+         updateFields.name = name;
+      }
+      if (categoryId.toString() !== ingredient.category.toString()) {
+         updateFields.category = categoryId;
+      }
 
-        // Check if there are changes in the ingredient's name, category, or image
-        const updateFields = {};
-        if (name !== ingredient.name) {
-            updateFields.name = name;
-        }
-        if (categoryId.toString() !== ingredient.category.toString()) {
-            updateFields.category = categoryId;
-        }
+      // If a new image is provided, update the category's image
+      if (req.file) {
+         await categoryController.updateCategoryImage(categoryId, req.file);
+      }
 
-        // If a new image is provided, update the category's image
-        if (req.file) {
-            await categoryController.updateCategoryImage(categoryId, req.file);
-        }
-
-        // Update the ingredient if there are changes
-            const resultIngredient = await Ingredient.findByIdAndUpdate(ingredientId, updateFields, { new: true });
-            res.redirect('/ingredients?success=Ingredient+updated+successfully');
-    } catch (error) {
-        res.redirect('/ingredients?error=true&message=' + encodeURIComponent(error.message));
-    }
-}
-
-
-
+      // Update the ingredient if there are changes
+      const resultIngredient = await Ingredient.findByIdAndUpdate(
+         ingredientId,
+         updateFields,
+         { new: true }
+      );
+      res.redirect('/ingredients?success=Ingredient+updated+successfully');
+   } catch (error) {
+      res.redirect(
+         '/ingredients?error=true&message=' + encodeURIComponent(error.message)
+      );
+   }
+};
