7f75677db0192773ba05f068efce385dafe7b464
s3977794 <s3977794@rmit.edu.vn>
2024-05-12 12:53:29 +0700

    chore: Refactor checkVerifyOTP middleware to simplify token validation logic

--- middlewares/checkVerifyOTP.js
+++ middlewares/checkVerifyOTP.js
@@ -2,25 +2,19 @@ const UserModel = require('../models/User');
 
 const checkVerifyOTP = async (req, res, next) => {
    try {
-      // Check if the user is signed in and otpVerify is true
       if (req.user && req.user.otpVerify === true) {
-         next(); // Allow access to the route
+         next(); 
       } else {
-         // Check if a valid token exists in the request parameters
-         const token = req.query.token; // Assuming token is included in the query parameters
+         const token = req.query.token; 
          if (token) {
-            // Check if the token is valid and not expired
             const user = await UserModel.findOne({ token });
             if (user && user.otpVerify === true) {
-               // Proceed with the reset password process
                next();
             } else {
-               // Invalid or expired token, redirect to reset password page
                req.flash('error', 'Invalid or expired token');
                return res.redirect('/account/profile/resetPassword');
             }
          } else {
-            // No valid token provided, redirect to reset password page
             req.flash('error', 'Please provide a valid token');
             return res.redirect('/account/profile/resetPassword');
          }
