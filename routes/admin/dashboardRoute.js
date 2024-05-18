a58b262e74da8e399d9c33405df30690b1e2933d
s3977794 <s3977794@rmit.edu.vn>
2024-05-18 13:20:00 +0700

    chore: Update dashboard route to display total users, recipes, ingredients, and cuisines

--- routes/admin/dashboardRoute.js
+++ routes/admin/dashboardRoute.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Ingredient = require('../../models/Ingredient');
+const Recipe = require('../../models/Recipe');
+const Cuisine = require('../../models/Cuisine');
+const User = require("../../models/User");
 
 const checkAdmin = require('../../middlewares/checkAdmin');
 const connectEnsureLogin = require('connect-ensure-login');
 
-router.get('/',connectEnsureLogin.ensureLoggedIn({redirectTo:'/signin/admin'}),checkAdmin, (req, res) => {
+router.get('/',connectEnsureLogin.ensureLoggedIn({redirectTo:'/signin/admin'}),checkAdmin, async (req, res) => {
+    const users = (await User.find({})).length;
+    const recipes = (await Recipe.find({})).length;
+    const ingredients = (await Ingredient.find({})).length;
+    const cuisines = (await Cuisine.find({})).length;
     res.render('admin/dashboard', {
         layout: 'layouts/admin/defaultLayout',
-        currentPage: 'dashboard'
-        ,user: req.user
-    });    
+        currentPage: 'dashboard',
+        user: req.user,
+        users,
+        recipes,
+        ingredients,
+        cuisines
+    });
 });
 
-module.exports = router
+module.exports = router;
